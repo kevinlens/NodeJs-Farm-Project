c6b7df68eecc4f0e5cf32fd03ef082d8d1beff8a
KTran-Tech <kevinmt191919@gmail.com>
2020-05-18 13:25:40 -0500

    Done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-
+const replaceTemplate = require('./modules/replaceTemplate');
 
 //--------------------------------------------
 //Files
@@ -39,20 +39,6 @@ const url = require('url');
 //Servers
 //------
 
-const replaceTemplate = (temp, product) =>{
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
-
-    return output;
-}
-
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
@@ -68,15 +54,19 @@ const dataObj = JSON.parse(data);
 
 
 
+
+
 /*create a server when you load it on webpage and send a 'req(request)' to that 
 page directory to get back a 'res(response)' object
 to which you could manipulate to modify the page*/
 const server = http.createServer((req, res) =>{
-    const pathName = req.url
+
+    const {query, pathname} = url.parse(req.url, true);
 
 
     //Overview Page
-    if(pathName === '/' || pathName === '/overview'){
+    if(pathname === '/' || pathname === '/overview'){
+
         //Allows html elements to be utilizied 
         res.writeHead(200, {'Content-type': 'text/html'})
         //loop through the parsed json text file
@@ -86,11 +76,16 @@ const server = http.createServer((req, res) =>{
         res.end(output);
      
     //Product Page        
-    } else if(pathName === '/product'){
-        res.end('This is the PRODUCT');
+    } else if(pathname === '/product'){
+        res.writeHead(200, {'Content-type': 'text/html'})
+
+        const product = dataObj[query.id]
+        const output = replaceTemplate(tempProduct, product)
+
+        res.end(output);
        
     //API        
-    } else if(pathName === '/api'){ 
+    } else if(pathname === '/api'){ 
         //This turns the json text into an actual open api database page
         res.writeHead(200, {'Content-type': 'application/json'})
         res.end(data);
