c71b9ca57f431e4cf0cf6810e6af6f8c16c68bbb
KTran-Tech <kevinmt191919@gmail.com>
2020-05-17 23:09:58 -0500

    Added a global function

--- index.js
+++ index.js
@@ -39,11 +39,32 @@ const url = require('url');
 //Servers
 //------
 
+const replaceTemplate = (temp, product) =>{
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-orgranic')
+    
+}
+
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+
 //this is just plain text response sent back after being read
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 
 // JSON file, is just text, written with JavaScript object notation
 const dataObj = JSON.parse(data);
+
+
 
 
 /*create a server and and send a 'req(request)' to that 
@@ -52,14 +73,27 @@ to which you could manipulate to modify the page*/
 const server = http.createServer((req, res) =>{
     const pathName = req.url
 
+
+    //Overview Page
     if(pathName === '/' || pathName === '/overview'){
-        res.end('This is the OVERVIEW');
+        //Allows html elements to be utilizied 
+        res.writeHead(200, {'Content-type': 'text/html'})
+        //loop through the parsed json text file
+        const cardsHtml = dataObj.map(item => replaceTemplate(tempCard, item))
+
+        res.end(tempOverview);
+     
+    //Product Page        
     } else if(pathName === '/product'){
         res.end('This is the PRODUCT');
+       
+    //API        
     } else if(pathName === '/api'){ 
         //This turns the json text into an actual open api database page
         res.writeHead(200, {'Content-type': 'application/json'})
         res.end(data);
+
+    //Not found        
     } else {
         //Allows html elements to be utilizied 
         res.writeHead(404, {
